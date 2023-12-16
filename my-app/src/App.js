59bfc94b3f3047a6fac77c93c06447666c850a41
caleb1711 <>
2023-12-16 10:12:45 +0000

    create an onClick button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
+   
     <div className={css.App}>
-      {/* Add your components here */}
+        <StatefulGreeting greeting="I'm a top Gee!" name="Caleb"/>
+      {/* < div className = {css.sidebar} /> */}
+
       <Sidebar />
+      
     </div>
   );
 }
